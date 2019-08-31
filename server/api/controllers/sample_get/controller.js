b97dd23b36281b4a604af1cc64706a3d6ddc257a
Joshua <jhorwitz28739@gmail.com>
2019-08-31 13:24:12 -0400

    added scripts folder and error handling on controller/service

--- server/api/controllers/sample_get/controller.js
+++ server/api/controllers/sample_get/controller.js
@@ -3,10 +3,12 @@ import SampleService from '../../services/sample.service';
 class SampleController {
   sampleEndpoint(req, res) {
     SampleService
-      .get()
-      .then((r) => {
-        res.json(r)
-      });
+    .get()
+    .then((r) => {
+      res.json(r)
+    }).catch((err) => {
+      res.status(500).send();
+    });
   }
 }
 
--- server/api/services/sample.service.js
+++ server/api/services/sample.service.js
@@ -1,16 +1,21 @@
 import { Pool } from 'pg';
+import LOGGER from '../../common/logger';
 
 class SampleService {
   get() {
-    return Promise.resolve({"response": "hello world"});
-    // const pool = new Pool();
-    // return pool.query('SELECT NOW()', (err, res) => {
-    //   console.log(err, res);
-    //   pool.end();
-    //   return {
-    //     "response": "Hello World"
-    //   };
-    // });
+    // return Promise.resolve({"response": "hello world"});
+    const pool = new Pool();
+    return pool.query('SELECT NOW()').then((res) => {
+      LOGGER.info(`Successfuly retrieved record from database: ${res}`);
+      pool.end();
+      return {
+        "response": "Hello World"
+      };
+    })
+    .catch((err) => {
+      LOGGER.error(`Error connecting to database: ${err}`);
+      throw err;
+    });
   }
 }
 
